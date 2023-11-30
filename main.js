279dc73da9f248d89dd8380e28b2451f98eda0f7
Tengo <t596256969@gmail.com>
2023-11-30 21:01:53 +0400

    2569

--- main.js
+++ main.js
@@ -2,13 +2,16 @@
 const span = document.querySelector('.span');
 const h2 = document.querySelector('h2');
 const btn = document.querySelector('button');
+const img = document.querySelector('.img1')
+const svg = document.querySelector('.img2')
 
-btn.addEventListener('click', function() {
+
+btn.addEventListener('click', function () {
     fetch('https://api.adviceslip.com/advice')
         .then(res => res.json())
         .then(data => {
-            const {id, advice} = data.slip;
-            
+            const { id, advice } = data.slip;
+
             h2.textContent = advice;
             span.textContent = id;
         })
@@ -16,10 +19,13 @@ btn.addEventListener('click', function() {
 
     btn.disabled = true;
     btn.style.cursor = 'not-allowed';
-  
+    svg.classList.add("pause")
+    img.classList.add("pause1")
+    h2.innerHTML = "loading..."
 
 
-    setTimeout(function() {
+
+    setTimeout(function () {
         btn.disabled = false;
         btn.style.cursor = 'pointer';
     }, 1000);
